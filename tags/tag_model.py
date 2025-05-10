6342cb94ee0933a53875ef267639ed8737bfc26b
Derek Rosenzweig <dr2633@nyu.edu>
2025-05-10 15:57:19 -0700

    Adding repository structure

---
+++ tags/tag_model.py
@@ -0,0 +1,34 @@
+### chronologue-docs
+
+- Review mdx files and revise structure of pages 
+- Complete mdx for new concepts and foundations
+- Datasets and use cases 
+
+### chronologue 
+
+- Review scripts and compare with sdk 
+
+### chronologue-sdk
+
+- Developing initial scripts and schema
+- Provide a high level overview with TOC 
+
+- Conversation history (daily, weekly, monthly)
+
+
+### Documentation Review 
+
+- TypeScript/Java review 
+- pydantic
+- FastAPI 
+- API review more generally 
+- Deep focus on the structure of calendar applications
+- Review calendar APIs and interfaces (Asana example)
+- Review chat interfaces 
+- GPS history and RAND report 
+
+
+
+
+
+
